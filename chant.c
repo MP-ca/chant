80ce451d000a8c1e50bad97ea3aa225d4243c057
MP-ca <132527844+MP-ca@users.noreply.github.com>
2025-09-27 12:08:57 -0400

    - Improved checks for valid integers

--- chant.c
+++ chant.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <ctype.h>
 
 int main(void) {
-    int repeat;
+    int repeat; // User defined integer for chant repetition
     int i = 1;
 
     // Assign user input to the repeat variable
@@ -9,21 +10,23 @@ int main(void) {
     scanf("%d", &repeat);
     printf("\n");
 
-    // Check if the user's input is valid
-    if (repeat < 1) {
-        printf("Error: Not a valid integer\n");
-        return 1;
-    }
-
     // Repeat the chant as specified by the user
-    while (i <= repeat) {
+    for (isdigit(repeat); i <= repeat; i++) {
         printf("one!\n");
         printf("two!\n");
         printf("three!\n");
         printf("four!\n");
         printf("(%d)\n\n", i);
-        i++;
     }
 
-    return 0;
+    // Print an error if the input is invalid
+    if (i == 1) {
+        printf("Error: Not a valid integer\n");
+        return 1;
+    }
+
+    // Return 0 if chant executes successfully
+    else {
+        return 0;
+    }
 }
